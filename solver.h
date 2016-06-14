5f346d50e5acd4315beea294734d91c4e517a5f1
Fabian Engels <fe@chiari.de>
2016-06-14 09:06:48 +0200

    [Task] First solver refactor

--- solver.h
+++ solver.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "types.h"
+
+
+#define MATRIX_SIZE 9
+#define RAND(max) (rand() % max + 1)
+
+/* Funktionsprototypen */
+int sudokuPruefung( int[][ MATRIX_SIZE ] );
+int loeseSudoku( int[][ MATRIX_SIZE ] );
+int zifferPruefung( int[][ MATRIX_SIZE ], int, int, int );
+int zaehleLeereFelder( int[][ MATRIX_SIZE ] );
+void generiereSudoku( int[][ MATRIX_SIZE ], int );
