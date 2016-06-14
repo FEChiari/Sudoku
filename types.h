346007d23945911e6f211fd0a7b9be2c9aeed0c0
Fabian Engels <fe@chiari.de>
2016-06-14 13:37:06 +0200

    [Task] Add new types

--- types.h
+++ types.h
@@ -7,7 +7,12 @@
 #define AUTHOR "Werba, Schorn, Krämer, Prinz, Engels"
 
 typedef unsigned char u8;
+typedef unsigned short u16;
 typedef unsigned int u32;
+
+typedef char s8;
+typedef short s16;
+typedef int s32;
 
 enum eGameState
 {
