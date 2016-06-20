689c1044ba4fd312fec131b4014d89e5f207c8b1
Fabian Engels <fe@chiari.de>
2016-06-20 09:06:54 +0200

    [Task] Update ingame screenstate

--- state_ingame.c
+++ state_ingame.c
@@ -40,18 +40,15 @@ void ScreenState_Ingame( struct sGame* nGame )
   box( right_container, WA_VERTICAL, WA_HORIZONTAL );
   WINDOW* right_inner = derwin( right_container, right_container->_maxy - 2, right_container->_maxx - 2, 1, 1 );
 
+  mvwaddstr( right_inner, 0, 1, "Hotkeys" );
+  mvwaddstr( right_inner, 2, 1, "LEER - Zahl vorschlagen" );
+  mvwaddstr( right_inner, 4, 4, "H - Regeln" );
+  mvwaddstr( right_inner, 6, 2, "DEL - Ausgewähltes Feld" );
+  mvwaddstr( right_inner, 7, 8, "löschen" );
+
   mvwaddstr( nGame->whnd, right_container->_begy + right_container->_maxy + 1, right_container->_begx + right_container->_maxx - 15, "ESC: Hauptmenü" );
 
-
-
-
-
-
-
-
-
-
-  Ingame_RenderField( left_inner, 2, 1, &nGame->gameState.field[0][0] );
+  Ingame_RenderField( left_inner, 2, 1, &nGame->gameState.field[ 0 ][ 0 ] );
 
   u8 playing = 1;
   time_t time_then = time( NULL );
@@ -129,112 +126,9 @@ void ScreenState_Ingame( struct sGame* nGame )
 
   delwin( right_inner );
   delwin( right_container );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //int iSudoku[9][9];
-  //generiereSudoku(iSudoku, (int)nGame->difficultyLvl);
-  //WINDOW *ingameContainer = subwin(nGame->whnd, 21 , 39, 1, 1);
-  //box(ingameContainer, WA_VERTICAL, WA_HORIZONTAL);
-  //struct sFieldSet SpielFeld;
-  //struct sFieldDef zahlenFeld[9][9];
-  //
-  ////Spielfeld
-  //SpielFeld.dimension.x = 20;
-  //SpielFeld.dimension.y = 38;
-  //SpielFeld.position.x = 1;
-  //SpielFeld.position.y = 1;
-  //SpielFeld.label = "Sudoku 0.2.0";
-  //SpielFeld.fields = &zahlenFeld[0][0];
-  //SpielFeld.whnd = derwin(ingameContainer, SpielFeld.dimension.x, SpielFeld.dimension.y, 1, 1);
-  //
-  ////Felder
-  //char chelp[3];
-  //for (int i = 0; i < 9; i++)
-  //{
-  //    for (int j = 0; j < 9; j++)
-  //    {
-  //        zahlenFeld[i][j].dimension.x = 4;
-  //        zahlenFeld[i][j].dimension.y = 2;
-  //        zahlenFeld[i][j].position.x = j * 4;
-  //        zahlenFeld[i][j].position.y = i * 2;
-  //        zahlenFeld[i][j].type = INPUT_NUMERIC;
-  //        zahlenFeld[i][j].whnd = derwin(SpielFeld.whnd, zahlenFeld[i][j].dimension.y, zahlenFeld[i][j].dimension.x, zahlenFeld[i][j].position.y, zahlenFeld[i][j].position.x);
-  //        //Rahmen zeichen
-  //        mvwaddstr(zahlenFeld[i][j].whnd, 0, 0, "+");
-  //        mvwaddstr(zahlenFeld[i][j].whnd, 0, 1, "-");
-  //        mvwaddstr(zahlenFeld[i][j].whnd, 0, 2, "-");
-  //        mvwaddstr(zahlenFeld[i][j].whnd, 0, 3, "-");
-  //        mvwaddstr(zahlenFeld[i][j].whnd, 1, 0, "|");
-  //        //Werte einsetzen
-  //        _itoa(iSudoku[i][j], chelp, 10);
-  //        if (chelp[0] != '0')
-  //        {
-  //            wattron(zahlenFeld[i][j].whnd, COLOR_PAIR(3));
-  //            mvwaddstr(zahlenFeld[i][j].whnd, 1, 2, chelp); 
-  //            wattroff(zahlenFeld[i][j].whnd, COLOR_PAIR(3));
-  //        }
-
-  //        wrefresh(zahlenFeld[i][j].whnd);
-  //    }
-  //    mvwaddstr(SpielFeld.whnd, i*2, 36, "+");
-  //    mvwaddstr(SpielFeld.whnd, i*2+1, 36, "|");
-  //    mvwaddstr(SpielFeld.whnd, 18, i*4, "+---");
-  //}
-  //mvwaddstr(SpielFeld.whnd, 18, 36, "+");
-  //wrefresh(ingameContainer);
-  //wmove(SpielFeld.whnd, 2, 3);
-  //wrefresh(SpielFeld.whnd);
-  //
-  //u8 handleInput = 1;
-  //do
-  //{
-  //    char key = Utility_WGetKeyCode(nGame->whnd);
-
-  //    switch (key)
-  //    {
-  //    case 0x1B: // ESC-key
-  //        handleInput = 0;
-  //        nGame->prevScreenState = nGame->screenState;
-  //        nGame->screenState = SCREEN_MAIN_MENU;
-  //        break;
-  //    }
-
-  //} while (handleInput);
-
-  //delwin(SpielFeld.whnd);
-  //delwin(ingameContainer);
 }
 
-void Ingame_RenderField( WINDOW* nTargetWindow, u8 nXOffset, u8 nYOffset, u8* nValues )
+void Ingame_RenderField( WINDOW* nTargetWindow, u8 nXOffset, u8 nYOffset, struct sSudokuField* nFields )
 {
 
   mvwaddstr( nTargetWindow, 0 + nYOffset, 0 + nXOffset, " 9   9   9   9   9   9   9   9   9 " );
